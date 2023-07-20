fc5765a7026df08f83fb90f54b13f6232b9bdc74
Maureen_g <gamradicalsix@gmail.com>
2023-07-20 14:42:44 -0500

    Wrote JS for messages to appear based on input, added conditionals and array for letters guessed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const guessedLetters = document.querySelector(".guessed-letters");
 //Guess button
 const guessButton = document.querySelector(".guess");
 //Input where player guesses letter
-const letterGuess = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 //Word in progress appears
 const inProgress = document.querySelector(".word-in-progress");
 //Remaining guesses paragraph display
@@ -11,18 +11,18 @@ const remainingGuesses = document.querySelector(".remaining");
 //Span inside remaining guesses paragraph showing guesses left
 const guessesSpan = document.querySelector(".remaining span");
 //Empty paragraph for messages after letter is guessed
-const guessedPara = document.querySelector(".message");
+const message = document.querySelector(".message");
 //Hidden button prompting player to play again
 const playAgain = document.querySelector(".play-again");
 
 //Default word
 let word = "magnolia";
 
-let lettersGuessed = [];
+const lettersGuessed = [];
 
 //Display symbol placeholders for letters in game
  const placeholder = function (word) {
-    const letterPlaceholder = [""];
+    const letterPlaceholder = [];
     for (const letter of word) {
         console.log(letter);
         letterPlaceholder.push("●");
@@ -33,10 +33,40 @@ let lettersGuessed = [];
  placeholder(word);
 
  //Letter entered into the input field in the console when the Guess button is clicked. 
- guessButton.addEventListener("click", function(e) {
+ guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const guess = letterGuess.value;
-    console.log(guess);
-    letterGuess.value = "";
+    message.innerText = "";
+    const guess = letterInput.value;
+    const goodGuess = validateInput(guess);
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
  });
+
+ //Validating letters and return messages for different scenarios
+ const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Guess a letter!";
+    } else if (input.length > 1) {
+        message.innerText = "Only one letter at a time please.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Only enter a letter from A to Z please.";
+    } else {
+        return input;
+    }
+ };
+
+ const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (lettersGuessed.includes(guess)){
+        message.innerText = "You already fuessed that letter Sillypuss. Try again.";
+    } else {
+        lettersGuessed.push (guess);
+        console.log(lettersGuessed);
+    }
+};
+
+
 
